577e00ef439dcc6d0f0fbe2372bfdf4e8b62b314
Ibrahim <ibrahim.fakhry93@yahoo.com>
2025-05-11 21:26:10 +0300

    algo analysis

---
+++ 03 Algorithm Analysis/code.js
@@ -0,0 +1,35 @@
+//! 05 – Algorithms Analysis – Introduction
+
+//* algorithm analysis measures approximately code performance, according to infinite number number of inputs
+
+//! 06 – Algorithms Analysis – Calculus Function
+
+//* function in math: is a mathematical relation between two variables (x , y)
+//* whenever x value changes, y will be changed.
+
+//* المجال للدالة هو القيم المحتملة للبيانات الداخلة
+
+//* function domain: it's the possible values for inputs
+//* function range: it's the possible values for outputs
+
+//! 07 – Algorithms Analysis – Big O Notation
+
+//* notation: forms or symbols to describe group of values or operations
+
+//* Growth rate of a function, with respect to inputs growth and resulted outputs growth
+//* O === Order of the function
+
+//* base of log in computer science is 2
+//* base of log in math is 10
+
+//* log(n) < n
+
+//* log (16) = 4
+//* 2 ** 4 = 16
+
+//! 08 – Algorithms Analysis – Drawing Complexity classes on graph
+
+//* o(n) === number of inputs equals number of iterations that code will execute
+
+//* the more the graph inclines towards x axis, the better the order of code is
+//*  the more the graph inclines towards y axis, the worse the order of code is
