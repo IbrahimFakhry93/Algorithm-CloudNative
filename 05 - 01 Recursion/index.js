c55798e368d9a253a99191e5f16987b30dc10cb1
Ibrahim <ibrahim.fakhry93@yahoo.com>
2025-05-12 21:24:46 +0300

    algo2

--- 05 - 01 Recursion/index.js
+++ 05 - 01 Recursion/index.js
@@ -1,1 +1,18 @@
 //! 10 – Recursion
+
+//& Code Segment
+//* program memory structure (code segment) splits into:
+//* code section: binary form of compiled code
+//* stack: store primitive data types and pointer of reference data types
+//* functions store its variables including parameters in the stack frame in the stack
+
+//* heap: store reference data types
+
+//*================================
+
+//& Recursion:
+
+//* function calls itself recursively.
+//* It relies on a base case to stop the recursive calls and prevents infinite recursion.
+//* when the function calls itself, it calls itself with parameters approach to base condition
+//! to avoid code exception (error) which is called stack overflow
