a3b4f4a6ef8ffe8dd3d1cd0b52f7f28135a74655
Ibrahim <ibrahim.fakhry93@yahoo.com>
2025-05-13 19:34:59 +0300

    algo2

--- 05 - 02 Merge Sort/index.js
+++ 05 - 02 Merge Sort/index.js
@@ -27,26 +27,98 @@
 
 //& Pseudo Code:
 //? Divide and Recursion:
-//* Define two functions: fn_mergeSort()
-//* Read arr[], start and end index inside fn_mergeSort()
+//* Define fn_mergerSort(arr,start,end);
+//* fn inputs: arr[], start and end index inside fn_mergeSort()
 //* if (start === end) break the recursion
-//* calculate the midpoint = (start + end) / 2
-//* call the fn_mergeSort() recursively
-//* merge the two splitted arrays
+//* calculate the midpoint = (end - start) / 2
+//* call the fn_mergeSort() recursively twice with different parameters to split arr[]
+//* call merge fn to split the two arrays
 
 //^================
 
 //? Merge part:
 
-//* Define fn_merge()
-//* Read the array (x), start, midpoint and end index
+//* Define fn_merge(arr,start,midpoint,end);
+//* fn inputs: the arr[], start, midpoint and end index
 //* create new arras for each side
-//* compare all items in the array and sort in the original array
-//* move remain items in each array to the original array
+//* while loop to compare all items in the array and sort in the original array
+//* move remain items in each array to the original array as they are
+//* because at this point they are certainly sorted
 
 //& code:
 
-//* two functions: fn_mergeSort(), fn_merge()
+//* Define fn_mergeSort()
+function mergeSort(arr, start, end) {
+  //* fn inputs: arr[], start and end index inside fn_mergeSort()
 
-//* fn_mergerSort(arr,start,end);
-//* fn_mergerSort(arr,start,midpoint);
+  //* if (start === end) break the recursion
+  if (start === end) return;
+  //* calculate the midpoint = (start + end) / 2
+  let midpoint = Math.floor((end - start) / 2);
+  //* call the fn_mergeSort() recursively twice with different parameters
+  mergeSort(arr, start, midpoint); //* merge sort for left
+  mergeSort(arr, midpoint + 1, end); //* merge sort for right
+
+  //* merge the two splitted arrays after recursion done
+  merge(arr, start, midpoint, end);
+}
+
+//* Define fn_merge(arr,start,midpoint,end);
+function merge(arr, start, midpoint, end) {
+  //* fn inputs: the arr[], start, midpoint and end index
+
+  //* create new arrays for each side
+  const right_arr = [];
+  const left_arr = [];
+
+  //* while loop to compare all items in the array and sort in the original array
+  let i,
+    j = 0;
+  let k = start;
+
+  //* i: for left array
+  //* j: for right array
+  //* k: for original array (arr)
+  while (left_arr.length > i && right_arr.length > j) {
+    //* compare
+    if (right_arr[j] <= left_arr[i]) {
+      //* sort (place the smaller in the original array at index k)
+      arr[k] = right_arr[j];
+      j++;
+    }
+    //* compare
+    else {
+      //* sort (place the smaller in the original array at index k)
+      arr[k] = left_arr[i];
+      i++;
+    }
+
+    k++; //* after compare finish, move to the next index in the original array
+    //* to place the next smaller number
+  }
+
+  //* move remain items in each array to the original array as they are (without comparing)
+  //* by doing two separate while loops
+
+  //^ note:
+  //* one of these next loops only will be executed
+  //* because one of the arrays will be finished sorted and other array still have items to loop on
+  while (left_arr.length > i) {
+    //^ note: no need to compare again
+    //* because at this point they are certainly sorted
+    arr[k] = left_arr[i]; //* k, i values are k++ and i++ from the last while loop above
+    i++;
+    k++;
+  }
+
+  while (right_arr.length > j) {
+    arr[k] = right_arr[j];
+    j++;
+    k++;
+  }
+}
+
+const arr = [9, 5, 1, 4];
+console.log(arr);
+mergeSort(arr, 0, arr.length - 1);
+console.log(`After sort: ${arr}`);
